d2172cac3d25d02f9df0d6e6e22726fdbd3088f1
chris <chris4403@gmail.com>
2011-10-21 23:42:44 +0900

    atndのAPIを叩いて表示

--- app.js
+++ app.js
@@ -43,5 +43,61 @@ app.post('/', function(req, res){
   });
 });
 
+// atnd events api
+app.get('/events', function(req, res){
+
+  var http = require('http');
+  var options = {
+      host : 'api.atnd.org',
+      port : 80,
+      path : '/events/?format=json',
+  };
+  var result = {};
+  http.get(options, function(response) {
+    response.setEncoding('utf8');
+    var json = "";
+    response.on('data',function(d) {
+        json += d;
+    }).on('end',function() {
+        result = JSON.parse(json);
+        res.render('events', {
+          title: 'atnd event results',
+          total_events : result.results_available,
+          events : result.events
+        });
+    });
+  }).on('error',function(e) {
+      console.log(e);
+  });
+
+});
+
+app.get('/event', function(req, res) {
+  var eventId = req.query.event_id;
+
+  var http = require('http');
+  var options = {
+      host : 'api.atnd.org',
+      port : 80,
+      path : '/events/?format=json&event_id=' + eventId,
+  };
+  var result = {};
+  http.get(options, function(response) {
+    response.setEncoding('utf8');
+    var json = "";
+    response.on('data',function(d) {
+        json += d;
+    }).on('end',function() {
+        result = JSON.parse(json);
+        res.render('event', {
+          title: 'atnd event',
+          event : result.events[0]
+        });
+    });
+  }).on('error',function(e) {
+      console.log(e);
+  });
+});
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
